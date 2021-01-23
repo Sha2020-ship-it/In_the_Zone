70f7a6b5b8f91d1454363e7afdce5529de43c3f1
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2021-01-23 11:55:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import os
+import winsound
+import ctypes as types
+import tkinter as tk
+import winreg
+
+root = tk.Tk()
+root.title('IN THE ZONE....')
+
+def block_sites_label():
+    mylabel = tk.Label(root, text = "Starting to block sites").grid(row = 2, column = 0)
+
+def unblock_sites_label():
+    mylabel2 = tk.Label(root, text="Starting to unblock sites").grid(row = 7, column = 0)
+
+focus_mode_off = tk.Button(root, text = "Turn Focus-Mode off", padx = 55, pady = 45, command = unblock_sites_label, bg = "red")
+focus_mode_on = tk.Button(root, text = "Turn Focus-Mode on", padx = 50, pady = 45, command = block_sites_label, bg = "green")
+
+e = tk.Entry(root, width = 70)
+
+e.insert(0, "Add sites that you want to unblock. Seperate with comma ','")
+
+e.grid(row = 0, column = 0, columnspan = 4)
+
+focus_mode_on.grid(row = 2, column = 0)
+focus_mode_off.grid(row = 2, column = 1)
+
+root.mainloop()
 
