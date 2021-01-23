7184dce98ab85a7eac73489e4dc5e88c8ed8cf1a
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2021-01-23 16:57:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import getpass
 import ctypes as types
 import tkinter as tk
 import winreg
+from time import *
+from datetime import *
 import random
 
 INTERNET_SETTINGS = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
@@ -56,12 +58,25 @@ def focus_mode():
 
     e.insert(0, "Add sites that you want to unblock. Seperate with comma ','")
 
+    unblock = str(e.get())
+    unblock.split(',')
+    for website in unblock:
+        if 'https' in website:
+            set_key('ProxyServer', u'{}:443'.format(website))
+            set_key('ProxyOverride', u'{}:443'.format(website))
+        elif 'http' in website:
+            set_key('ProxyServer', u'{}:80'.format(website))
+            set_key('ProxyOverride', u'{}:80'.format(website))
+        elif 'ftp' in website:
+            set_key('ProxyServer', u'{}:21'.format(website))
+            set_key('ProxyOverride', u'{}:21'.format(website))
+
     e.grid(row = 0, column = 0, columnspan = 4)
 
     focus_mode_on.grid(row = 2, column = 0)
     focus_mode_off.grid(row = 2, column = 1)
 
-    root.mainloop()
+    root1.mainloop()
 
 def block():
     root2 = tk.Tk()
@@ -72,22 +87,41 @@ def block():
     Websites.place(x= 140,y = 60)
 
     def Blocker():
-        website_lists = Websites.get(1.0,END)
-        Website = list(website_lists.split(","))
-        with open (host_path , 'r+') as host_file:
-            file_content = host_file.read()
-            for website in Website:
-                if website in file_content:
-                    tk.Label(root2, text = 'Already Blocked' , font = 'arial 12 bold').place(x=200,y=200)
-                    pass
-                else:
-                    host_file.write(ip_address + " " + website + '\n')
-                    tk.Label(root2, text = "Blocked", font = 'arial 12 bold').place(x=230,y =200)
+        host_path = r"C:\\Windows\\System32\\drivers\\etc\\hosts"  
+        redirect = "127.0.0.1"  
+
+        while True:  
+            if datetime(datetime.now().year,datetime.now().month,datetime.now().day,9)<datetime.now()<datetime(datetime.now().year,datetime.now().month,datetime.now().day,17):  
+                with open(host_path,"r+") as fileptr:  
+                    content = fileptr.read()  
+                    for website in websites:  
+                        if website in content:  
+                            pass  
+                        else:  
+                            fileptr.write(redirect+"        "+website+"\n")  
+            else:  
+                with open(host_path,'r+') as file:  
+                    content = file.readlines();  
+                    file.seek(0)  
+                    for line in content:  
+                        if not any(website in line for website in websites):  
+                            file.write(line)  
+                    file.truncate()  
+    sleep(5)  
 
     block = tk.Button(root2, text = 'Block',pady = 5,command = Blocker ,width = 6, bg = 'red', activebackground = 'sky blue')
     block.place(x = 230, y = 150)
-
+    
     root2.mainloop()
+
+def menu():
+    root = tk.Tk()
+    root.title('Focus Mode Activation')
+
+    bring_focus = tk.Button(root, text = 'Go to Proxy Server', padx = 45, pady = 45, command = focus_mode).grid(row = 0, column = 0)
+    bring_block = tk.Button(root, text = 'Go to Website Blocker', padx = 45, pady = 45, command = block).grid(row = 0, column = 1)
+
+    root.mainloop()
 
 def password():
     x = str(random.randint(1000,9999))
@@ -98,15 +132,19 @@ def password():
     message = f"{x} is your password"
     s.sendmail("noreply123.InTheZone@gmail.com", emailreciev, message) 
     s.quit()
-    
+    code = input('Check your mail - Enter the code you got: ')
+    if code == x:
+        print('Accepted!')
+        print('You can acess the software.')
+        menu()
+    else:
+        for i in range(3):
+            if code != x:
+                print('Not accepted!')
+                code = input('Check your mail - Enter the code you got: ') 
+            elif code == x:
+                print('Accepted!')
+                print('You can acess the software.')
+                menu()
 
 password()
-
-if password() == True:
-    root = tk.Tk()
-    root.title('Focus Mode Activation')
-
-    bring_focus = tk.Button(root, text = 'Go to Proxy Server', padx = 45, pady = 45, command = focus_mode).grid(row = 0, column = 0)
-    bring_block = tk.Button(root, text = 'Go to Website Blocker', padx = 45, pady = 45, command = block).grid(row = 0, column = 1)
-
-    root.mainloop()
